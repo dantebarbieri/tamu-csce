b6a2389a695e6371b663e1d384aa4fcb559a21fc
pulchroxloom <dantevbarbieri@gmail.com>
2021-03-17 10:48:45 -0500

    Tweak print & Add srand

--- CSCE 221 - Data Structures and Algorithms/5 Trees/3 Binary Search Trees/main.cpp
+++ CSCE 221 - Data Structures and Algorithms/5 Trees/3 Binary Search Trees/main.cpp
@@ -7,11 +7,12 @@ using std::cout, std::endl;
 template <typename T>
 void print(typename BST<T>::Node* n) {
     if(n) {
-        cout << n->obj << endl;
+        cout << n->obj << ' ';
     }
 }
 
 int main() {
+    srand(time(NULL));
     BST<int> b;
     // BST<int>::print_separator = "\t\t";
     for(size_t i = 1; i <= 9; ++i)
